65443df887ab32bd413e0a48c4c01ea477c90c22
savinci <lowdeep1004@gmail.com>
2023-04-07 15:39:47 +0900

    musicblog

--- js/music-list.js
+++ js/music-list.js
@@ -9,207 +9,374 @@
 
 //paste it inside the array as more as you want music then you don't need to do any other thing
 
-let allMusic = [
-  {
-    name: "Snowman",  
-    artist: "Sia",
+let allMusic = [{
+    name: "A whiter shade of pale",
+    artist: "procol harum",
     img: "music-1",
     src: "music-1"
   },
   {
-    name: "Summer Days", 
-    artist: "Martin Garrix",
+    name: "I have been away too long",
+    artist: "george baker selection",
     img: "music-2",
     src: "music-2"
   },
   {
-    name: "Nappy roots",
-    artist: "Good Day",
+    name: "Soldier of fortune",
+    artist: "Deep purple",
     img: "music-3",
     src: "music-3"
   },
   {
-    name: "Gimme Gimme",
-    artist: "Johnny Stimson",
+    name: "Before the dawn",
+    artist: "Judas priest",
     img: "music-4",
     src: "music-4"
   },
   {
-    name: "Aubrey", 
-    artist: "BREAD",
+    name: "Shape of my heart",
+    artist: "Sting",
     img: "music-5",
     src: "music-5"
   },
   {
-    name: "Ho Ho Ho",
-    artist: "Sia",
+    name: "friendships",
+    artist: "Pascal letoublon",
     img: "music-6",
     src: "music-6"
   },
   {
-    name: "I Am Better Off", 
-    artist: "Wildson",
+    name: "Love yourself",
+    artist: "Justin bieber",
     img: "music-7",
     src: "music-7"
   },
   {
-    name: "Let It Be Me",  
-    artist: "Phil Everly",
+    name: "I will follow you",
+    artist: "Ricky nelson",
     img: "music-8",
     src: "music-8"
   },
   {
-    name: "Warm..Night", 
-    artist: "HONNE",
+    name: "Englishman in New york",
+    artist: "Sting",
     img: "music-9",
     src: "music-9"
   },
   {
-    name: "Thunderclouds", 
-    artist: "Sia",
+    name: "Live is life",
+    artist: "Opus",
     img: "music-10",
     src: "music-10"
   },
   {
-    name: "If", 
-    artist: "Sissel",
+    name: "Brother louie '98",
+    artist: "Modern talking",
     img: "music-11",
     src: "music-11"
   },
   {
-    name: "Cheap Thrills", 
-    artist: "Sia",
+    name: "Let it be me",
+    artist: "Phil everly",
     img: "music-12",
     src: "music-12"
   },
   {
-    name: "CandyCaneLane", 
-    artist: "Sia",
+    name: "If",
+    artist: "Sissel",
     img: "music-13",
     src: "music-13"
   },
   {
-    name: "The Greatest", 
-    artist: "Sia",
+    name: "Mo better blues",
+    artist: "Branford marsalis quartet",
     img: "music-14",
     src: "music-14"
   },
   {
-    name: "Stand by Me", 
-    artist: "Alexandra",
+    name: "Not going anywhere",
+    artist: "keren ann",
     img: "music-15",
     src: "music-15"
   },
   {
-    name: "Wishes", 
-    artist: "Emi Fujita",
+    name: "Knife",
+    artist: "Rockwell",
     img: "music-16",
     src: "music-16"
   },
   {
-    name: "Over The Rainbow", 
-    artist: "Debison",
+    name: "Love hurts",
+    artist: "Nazareth",
     img: "music-17",
     src: "music-17"
   },
   {
-    name: "In love sometimes", 
-    artist: "Elton John",
+    name: "Love of my life",
+    artist: "Queen",
     img: "music-18",
     src: "music-18"
   },
   {
-    name: "Count On Me", 
-    artist: "Jefferson Starship",
+    name: "Magic",
+    artist: "Pilot",
     img: "music-19",
     src: "music-19"
   },
   {
-    name: "You're Only Lonely", 
-    artist: "J.D. Souther",
+    name: "Thunderclouds",
+    artist: "Sia",
     img: "music-20",
     src: "music-20"
-  }, 
+  },
   {
-    name: "When we disco", 
-    artist: "J.Y.Park",
+    name: "Gimme Gimme",
+    artist: "Johnny stimson",
     img: "music-21",
     src: "music-21"
-  }, 
+  },
   {
-    name: "Love Yourself", 
-    artist: "Justin Bieber",
+    name: "Wishes",
+    artist: "Emi fujita",
     img: "music-22",
     src: "music-22"
-  }, 
+  },
   {
-    name: "Peaches", 
-    artist: "Justin Bieber",
+    name: "You are only lonely",
+    artist: "J.D.southerr",
     img: "music-23",
     src: "music-23"
-  }, 
+  },
   {
-    name: "Never Stop", 
-    artist: "Chilly Gonzales",
+    name: "Long long time",
+    artist: "Linda ronstadt",
     img: "music-24",
     src: "music-24"
-  }, 
+  },
   {
-    name: "The water is wide", 
-    artist: "Karla Bonoff",
+    name: "Snowman",
+    artist: "Sia",
     img: "music-25",
     src: "music-25"
-  }, 
+  },
   {
-    name: "Four strong winds", 
-    artist: "Neil Young",
+    name: "Peaches",
+    artist: "Justin bieber",
     img: "music-26",
     src: "music-26"
-  }, 
+  },
   {
-    name: "Lilac", 
-    artist: "IU",
+    name: "500 miles",
+    artist: "Peter Paul and Mary",
     img: "music-27",
     src: "music-27"
   },
   {
-    name: "Home Sweet Home", 
-    artist: "Car The Garden",
+    name: "The water is wide",
+    artist: "Karla bonoff",
     img: "music-28",
     src: "music-28"
-  }, 
+  },
   {
-    name: "For hesitant lover", 
-    artist: "JANNABI",
+    name: "You are always on my mind",
+    artist: "Willy nelson",
     img: "music-29",
     src: "music-29"
-  }, 
+  },
   {
-    name: "Knocking..heaven..", 
-    artist: "Nazareth",
+    name: "under pressure",
+    artist: "Queen",
     img: "music-30",
     src: "music-30"
-  } ,
- {
-    name: "Summer Night", 
-    artist: "JANNABI",
+  },
+  {
+    name: "Havana",
+    artist: "Camila cabello",
     img: "music-31",
     src: "music-31"
-  }, 
- {
-    name: "Tree", 
-    artist: "Car The Garden",
+  },
+  {
+    name: "Sisyphus",
+    artist: "Andrew bird",
     img: "music-32",
     src: "music-32"
-  }, 
-  
-   {
-    name: "Soldier of fortune", 
-    artist: "Deep Purple",
+  },
+
+  {
+    name: "Aubrey",
+    artist: "Bread",
     img: "music-33",
     src: "music-33"
+  },
+
+  {
+    name: "Bohemian rhapsody",
+    artist: "Queen",
+    img: "music-34",
+    src: "music-34"
+  },
+
+  {
+    name: "One more cup of coffee",
+    artist: "Bob dylan",
+    img: "music-35",
+    src: "music-35"
+  },
+
+  {
+    name: "count on me",
+    artist: "Jefferson starship",
+    img: "music-36",
+    src: "music-36"
+  },
+
+  {
+    name: "Stay",
+    artist: "Maurice Williams & The zodiacs",
+    img: "music-37",
+    src: "music-37"
+  },
+
+  {
+    name: "Four strong winds",
+    artist: "Neil young",
+    img: "music-38",
+    src: "music-38"
+  },
+
+  {
+    name: "I am better off",
+    artist: "Wildson feat.Lakesha nugent",
+    img: "music-39",
+    src: "music-39"
+  },
+
+  {
+    name: "You never can tell",
+    artist: "Chuck berry",
+    img: "music-40",
+    src: "music-40"
+  },
+
+  {
+    name: "Always with me",
+    artist: "Youmi kimura",
+    img: "music-41",
+    src: "music-41"
+  },
+
+  {
+    name: "In dreams",
+    artist: "Chuck berry",
+    img: "music-42",
+    src: "music-42"
+  },
+
+  {
+    name: "Dream lover",
+    artist: "Bobby darin",
+    img: "music-43",
+    src: "music-43"
+  },
+
+  {
+    name: "Stand by me",
+    artist: "Ben E.king",
+    img: "music-44",
+    src: "music-44"
+  },
+
+  {
+    name: "Knock'in on heavens door",
+    artist: "Bobby darin",
+    img: "music-45",
+    src: "music-45"
+  },
+
+  {
+    name: "Moves like jagger",
+    artist: "Maroon5",
+    img: "music-46",
+    src: "music-46"
+  },
+
+  {
+    name: "Only you",
+    artist: "Platters",
+    img: "music-47",
+    src: "music-47"
+  },
+
+  {
+    name: "Massachusetts",
+    artist: "Bee Gees",
+    img: "music-48",
+    src: "music-48"
+  },
+
+  {
+    name: "San francisco",
+    artist: "Scott mckenzie",
+    img: "music-49",
+    src: "music-49"
+  },
+
+  {
+    name: "Bad guy",
+    artist: "Villie Eilish",
+    img: "music-50",
+    src: "music-50"
+  },
+
+  {
+    name: "The rose",
+    artist: "Bette Midler",
+    img: "music-51",
+    src: "music-51"
+  },
+
+  {
+    name: "We all fall in love sometimes",
+    artist: "Elton john",
+    img: "music-52",
+    src: "music-52"
+  },
+
+  {
+    name: "If",
+    artist: "Taeyeon",
+    img: "music-53",
+    src: "music-53"
+  },
+
+  {
+    name: "My all",
+    artist: "Maria carey",
+    img: "music-54",
+    src: "music-54"
+  },
+
+  {
+    name: "Quando Quando Quando",
+    artist: "Michael buble",
+    img: "music-55",
+    src: "music-55"
+  },
+
+  {
+    name: "Every Second",
+    artist: "Mina Okabe",
+    img: "music-56",
+    src: "music-56"
+  },
+
+    {
+    name: "Serenade",
+    artist: "Franz Schubert",
+    img: "music-57",
+    src: "music-57"
   }
- 
+
 
   // like this paste it and remember to give comma after ending of this bracket }
   // {
